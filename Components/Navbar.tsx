05b21e2af172a75767df71c626914556beef92e4
JP-sDEV <jon.pablo80@gmail.com>
2022-08-23 13:48:56 -0400

    build: add Logo and styling to NavBar

---
+++ Components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React, { useContext } from 'react'
+import Search from '../public/assets/search.svg'
+// import Logo from '../public/assets/logo.svg'
+import Logo from '../public/icon.ico'
+
+import {AppContext, StoreContext} from '../Context/global'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+
+    const { locationVisible, setLocationVisible } = useContext(AppContext) as StoreContext
+
+    const onClick = () => {
+        setLocationVisible(!locationVisible)
+    }
+
+  return (
+    <div className='bg-secondaryCol rounded-md' >
+        <ul className="flex justify-between"> 
+            <li className="mr-6 my-auto flex">
+                <h1 className='ml-3 mr-1 my-auto'>Weather.me</h1>
+                <Image src={Logo} height={40} width={40}/>
+            </li>  
+              
+            <li className="mr-4 my-auto pt-1" onClick={() => onClick()}>
+                <Image src={Search} width={30}/>
+            </li>       
+        </ul>
+    </div>  
+  )
+}
+
+export default Navbar
