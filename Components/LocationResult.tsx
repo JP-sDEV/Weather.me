a0eeedefe627b01c12d90c1a0cd8de9bd619108b
JP-sDEV <jon.pablo80@gmail.com>
2022-08-23 13:45:38 -0400

    build: pass location API result to Location related components

---
+++ Components/LocationResult.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import React, { useContext } from 'react'
+import Pin from '../public/assets/pin.svg'
+import { AppContext, StoreContext } from '../Context/global'
+import { ParseTemps, ParseHours, ParsePrecip, ParseWind } from '../pages/api/Helpers/ParseData'
+
+
+type Props = {
+  formattedName: String,
+  cityName: String,
+}
+
+const LocationResult = (props: Props) => {
+
+  const { chartData, setLocation, setForecast, setLocationVisible, setChartData } = useContext(AppContext) as StoreContext
+
+  const onClick = async (name: string) => {
+    setLocation(name)
+    const locationQ = name.replace(/\s/g, "");
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "location": locationQ
+      }),
+    };
+  
+    let forecast: any;
+    await fetch(`api/Weather`, requestOptions)
+      .then(res => res.json())
+      .then(result => forecast=result.res)
+      .catch(error => console.log('error', error));
+
+    setForecast(forecast)
+    setChartData({
+      ...chartData,
+      time: ParseHours(forecast),
+      temp: ParseTemps(forecast),
+      precip:ParsePrecip(forecast),
+      wind: ParseWind(forecast),
+      })
+
+    setTimeout(() => {
+      setLocationVisible(false)
+    }, 500)
+  }
+
+  return (
+    <div className='flex my-2 p-2 bg-red-500 rounded-md' onClick={() => onClick(props.formattedName)}>
+      <Image src={Pin} height={40} width={40}/> 
+      <p className='my-auto'>{props.formattedName}</p>
+    </div>
+  )
+}
+
+export default LocationResult
---
+++ Components/LocationSearch.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useContext } from 'react'
+import {AppContext, StoreContext} from '../Context/global'
+import LocationResult from './LocationResult'
+
+type Props = {}
+
+const LocationSearch = (props: Props) => {
+  const [response, setResponse] = useState(undefined)
+
+  const { forecast, location, locationVisible, setLocation, setLocationVisible } = useContext(AppContext) as StoreContext
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "location": location 
+    }),
+  };
+
+  const viewable = {
+    position: "fixed",
+    display: "block",
+    width:"100%",
+    height:"100%",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    zIndex: 2, 
+    cursor: "pointer" 
+    }
+
+    const onClick = (e: any) => {
+      e.preventDefault()
+      setLocationVisible(false)
+    }
+
+    const onChange = (e: any) => {
+
+      setLocation(e.target.value)
+      
+      if (e.target.value.length >= 3) {
+        setTimeout(() => {
+        
+          fetch(`api/LocationSearch`, requestOptions)
+            .then(res => res.json())
+            .then(result => setResponse(result.res))
+            .catch(error => console.log('error', error));
+  
+        }, 500)
+
+      }
+    }
+ 
+  return (
+    <div style={viewable}>
+
+      <div className='flex-col mx-auto my-10 p-2 w-5/6 h-5/6 bg-indigo-500 rounded-md' >
+        <div className='flex justify-end'>
+          <h1 className='' onClick={(e) => onClick(e)}>
+            {(locationVisible && forecast) ? "X" : ""}
+          </h1>
+        </div>
+
+        <div className="flex">
+          <h1 className="m-auto">
+            Enter a City
+          </h1>
+        </div>
+
+        <div className='flex my-4'>
+          <input className='m-auto w-5/6 p-2 rounded-md' 
+                 type='text'
+                 name='location'
+                 placeholder="Toronto, ON"
+                 onChange={e => onChange(e)}
+                 value={ location }
+                 />
+        </div>
+
+        <div className='flex'>
+          <div className='m-auto w-5/6'>
+            {
+              (locationVisible && response) ? 
+                response.results.map(c => {
+                  return <LocationResult formattedName={c.formatted} cityName={c.city} key={c.place_id}/>
+                }) :
+                []
+            }
+          </div>
+        </div>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default LocationSearch
